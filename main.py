16183196cd752f31ffaa38ef6fa6f676cc2a2bd5
Yahya Saad (") <74425218+YahyaS3d@users.noreply.github.com>
2023-05-19 14:57:53 +0300

    throw warnings
    throw warnings and remove unused notes

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import warnings
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
 from sklearn.model_selection import train_test_split
+from sklearn.exceptions import ConvergenceWarning
 
-#from sklearn.metrics import accuracy_score
-
+# Suppress convergence warnings
+warnings.filterwarnings("ignore", category=ConvergenceWarning)
 
 # Read x data from CSV file
 x_df = pd.read_csv('ex2_x_data.csv', header=None)
@@ -25,7 +27,7 @@ num_classes = len(np.unique(y))
 models = []
 for i in range(num_classes):
     y_train_class = (y_train == i).astype(int)
-    model = LogisticRegression()
+    model = LogisticRegression(max_iter=1000)
     model.fit(x_train, y_train_class)
     models.append(model)
 
@@ -37,7 +39,6 @@ for i, model in enumerate(models):
 # Convert probabilities to class labels
 y_pred_class = np.argmax(y_pred, axis=1)
 
-
 # Set a larger value for the max_iter parameter
 clf = LogisticRegression(max_iter=1000)
 
@@ -48,9 +49,7 @@ clf.fit(x_train, y_train)
 y_pred = clf.predict(x_test)
 
 # Print the confusion matrix
-from sklearn.metrics import confusion_matrix
-# Compute confusion matrix and print results
-conf_mat = confusion_matrix(y_test, y_pred) #kxk = matrix up to 10 x 10
+conf_mat = confusion_matrix(y_test, y_pred)
 print("Confusion matrix:\n", conf_mat)
 
 # Calculate accuracy rate
@@ -58,49 +57,3 @@ accuracy = accuracy_score(y_test, y_pred)
 
 # Print the accuracy rate
 print("Accuracy rate:", accuracy)
-# import pandas as pd
-# from sklearn.model_selection import train_test_split
-# from sklearn.linear_model import LogisticRegression
-# import numpy as np
-# from sklearn.metrics import confusion_matrix
-#
-# # Read x data from file
-# x_df = pd.read_csv('ex2_x_data.csv', header=None)
-#
-# # Read y data from file
-# y_df = pd.read_csv('ex2_y_data.csv', header=None)
-#
-# # Convert dataframes to numpy arrays
-# x = x_df.values
-# y = y_df.values.flatten() # Make y a 1D array
-# # Split data into training and testing sets
-# x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
-#
-# # Create binary classifiers
-# classifiers = []
-# k = 10
-# for i in range(k):
-#     # Create binary target vector for class i
-#     y_binary = np.where(y_train == i, 1, 0)
-#
-#     # Train binary classifier for class i
-#     clf = LogisticRegression(random_state=0).fit(x_train, y_binary)
-#
-#     # Add binary classifier to list of classifiers
-#     classifiers.append(clf)
-#
-# # Predict class labels for test set
-# y_pred = np.zeros_like(y_test)
-# for i, clf in enumerate(classifiers):
-#     # Predict probabilities for class i
-#     y_prob = clf.predict_proba(x_test)[:, 1]
-#
-#     # Update predicted class labels for class i
-#     # y_pred[y_prob > threshold] = i
-#     y_pred[y_prob > 0.5] = i
-#
-# # Calculate confusion matrix
-# conf_mat = confusion_matrix(y_test, y_pred)
-#
-# # Print confusion matrix
-# print(conf_mat)
